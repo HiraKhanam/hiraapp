526aa0ababfcd0f763ce758455ff21dc34339ed2
tahaOsama <taha.alphasquad@gmail.com>
2022-06-17 20:32:03 +0500

    changes made

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 function index() {
   return (
@@ -8,52 +7,19 @@ function index() {
         <nav className="w-full border-b">
           <div className="py-5 md:py-0 container mx-auto px-6 flex items-center justify-between">
             <div aria-label="Home. logo" role="img">
-              <Image
-                className="w-12 md:w-auto"
-                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/centre_aligned_simple-Svg1.svg"
-                alt="logo"
-              />
+              <img className="w-12 md:w-auto" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/centre_aligned_simple-Svg1.svg" alt="logo" />
             </div>
             <div>
-              <button
-                oncClick="toggleMenu(true)"
-                className="dark:bg-white rounded sm:block md:hidden text-gray-500 hover:text-gray-700 dark:text-gray-200 focus:text-gray-700 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500"
-              >
-                <svg
-                  aria-haspopup="true"
-                  aria-label="open Main Menu"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="md:hidden icon icon-tabler icon-tabler-menu"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="#2c3e50"
-                  fill="none"
-                  strokeLinecap="round"
-                >
+              <button oncClick="toggleMenu(true)" className="dark:bg-white rounded sm:block md:hidden text-gray-500 hover:text-gray-700 dark:text-gray-200 focus:text-gray-700 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500">
+                <svg aria-haspopup="true" aria-label="open Main Menu" xmlns="http://www.w3.org/2000/svg" className="md:hidden icon icon-tabler icon-tabler-menu" width={24} height={24} viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round">
                   <path stroke="none" d="M0 0h24v24H0z" />
                   <line x1={4} y1={8} x2={20} y2={8} />
                   <line x1={4} y1={16} x2={20} y2={16} />
                 </svg>
               </button>
               <div id="menu" className="md:block lg:block hidden">
-                <button
-                  onClick="toggleMenu(false)"
-                  className="dark:bg-white rounded block md:hidden lg:hidden text-gray-500 hover:text-gray-700 dark:text-gray-200 focus:text-gray-700 dark:text-gray-200 fixed focus:outline-none focus:ring-2 focus:ring-gray-500 z-30 top-0 mt-6"
-                >
-                  <svg
-                    aria-label="close main menu"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#2c3e50"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
+                <button onClick="toggleMenu(false)" className="dark:bg-white rounded block md:hidden lg:hidden text-gray-500 hover:text-gray-700 dark:text-gray-200 focus:text-gray-700 dark:text-gray-200 fixed focus:outline-none focus:ring-2 focus:ring-gray-500 z-30 top-0 mt-6">
+                  <svg aria-label="close main menu" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" />
                     <line x1={18} y1={6} x2={6} y2={18} />
                     <line x1={6} y1={6} x2={18} y2={18} />
@@ -78,9 +44,7 @@ function index() {
                 </ul>
               </div>
             </div>
-            <button className="focus:outline-none lg:text-lg lg:font-bold focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 hidden md:block bg-transparent transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-8 py-1 sm:py-3 text-sm">
-              Sign In
-            </button>
+            <button className="focus:outline-none lg:text-lg lg:font-bold focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 hidden md:block bg-transparent transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-8 py-1 sm:py-3 text-sm">Sign In</button>
           </div>
         </nav>
         <div className="bg-gray-100 dark:bg-transparent">
@@ -91,52 +55,27 @@ function index() {
                 <span className="text-indigo-700">Websites</span>
                 You Want
               </h1>
-              <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
-                A professonal website drives sales. Create a beautiful website
-                to impress and engage new customers and establish your business
-                online{" "}
-              </p>
+              <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">A professonal website drives sales. Create a beautiful website to impress and engage new customers and establish your business online </p>
             </div>
             <div className="flex justify-center items-center">
-              <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 lg:text-xl lg:font-bold  rounded text-white px-4 sm:px-10 border border-indigo-700 py-2 sm:py-4 text-sm">
-                Get Started
-              </button>
-              <button className="ml-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-transparent transition duration-150 ease-in-out hover:border-indigo-600 lg:text-xl lg:font-bold  hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">
-                Live Demo
-              </button>
+              <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 lg:text-xl lg:font-bold  rounded text-white px-4 sm:px-10 border border-indigo-700 py-2 sm:py-4 text-sm">Get Started</button>
+              <button className="ml-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-transparent transition duration-150 ease-in-out hover:border-indigo-600 lg:text-xl lg:font-bold  hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">Live Demo</button>
             </div>
           </div>
         </div>
         <div className="w-full lg:px-0 px-4 py-16">
           <div className="w-full flex flex-col items-center justify-center">
             <div className="sm:1/4 lg:w-1/2 flex justify-center items-center flex-col xl:w-2/5">
-              <h1
-                role="heading"
-                className="lg:text-3xl xl:text-4xl text-2xl leading-6 lg:leading-9 f-m-w text-indigo-700  text-center font-bold pt-6"
-              >
+              <h1 role="heading" className="lg:text-3xl xl:text-4xl text-2xl leading-6 lg:leading-9 f-m-w text-indigo-700  text-center font-bold pt-6">
                 Subscribe to our Newsletter
               </h1>
-              <p
-                role="contentinfo"
-                className="lg:text-base lg:leading-6 text-sm font-light text-gray-800 dark:text-white text-center mt-2 lg:mt-3 f-m-m"
-              >
-                Sign Up for our weekly newsletter to get to know about exciting
-                offers, our latest products ,the latest industry updates. and
-                amazing offers delivered directly in your inbox.
+              <p role="contentinfo" className="lg:text-base lg:leading-6 text-sm font-light text-gray-800 dark:text-white text-center mt-2 lg:mt-3 f-m-m">
+                Sign Up for our weekly newsletter to get to know about exciting offers, our latest products ,the latest industry updates. and amazing offers delivered directly in your inbox.
               </p>
-              <div
-                className="sm:flex md:px-4 p-2 my-8 border border-indigo-700 dark:bg-gray-800  rounded w-full lg:w-4/5 mt-8 xl:mt-12 items-center "
-                aria-label="Input Email"
-              >
-                <input
-                  type="email"
-                  className="py-2 dark:bg-gray-800 dark:placeholder-gray-200 dark:text-white w-full md:w-7/12 lg:w-full text-base placeholder-gray-400 focus:outline-none"
-                  placeholder="Enter your email"
-                />
+              <div className="sm:flex md:px-4 p-2 my-8 border border-indigo-700 dark:bg-gray-800  rounded w-full lg:w-4/5 mt-8 xl:mt-12 items-center " aria-label="Input Email">
+                <input type="email" className="py-2 dark:bg-gray-800 dark:placeholder-gray-200 dark:text-white w-full md:w-7/12 lg:w-full text-base placeholder-gray-400 focus:outline-none" placeholder="Enter your email" />
                 <div className="w-full sm:w-5/12 sm:flex justify-end mt-4 sm:mt-0">
-                  <button className="w-full sm:w-auto f-m-m md:text-base text-base rounded font-normal text-white bg-indigo-700 md:py-2 md:px-8 px-4 py-2.5 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 leading-4 hover:opacity-90">
-                    Subscribe
-                  </button>
+                  <button className="w-full sm:w-auto f-m-m md:text-base text-base rounded font-normal text-white bg-indigo-700 md:py-2 md:px-8 px-4 py-2.5 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 leading-4 hover:opacity-90">Subscribe</button>
                 </div>
               </div>
             </div>
@@ -144,77 +83,42 @@ function index() {
         </div>
         <div className="mx-auto container lg:px-20 md:px-6 px-4 py-12">
           <div className="flex items-center justify-center flex-col">
-            <h1 className="lg:text-5xl text-3xl font-bold leading-10 text-gray-800 dark:text-white text-center">
-              Faces Behind Our Success
-            </h1>
-            <p className="text-base leading-6 text-center text-gray-600 dark:text-gray-200 mt-5 lg:w-5/12 sm:w-7/12">
-              A good idiom for kids is "It's raining cats and dogs." Kids know
-              what both cats and dogs are from an early age
-            </p>
+            <h1 className="lg:text-5xl text-3xl font-bold leading-10 text-gray-800 dark:text-white text-center">Faces Behind Our Success</h1>
+            <p className="text-base leading-6 text-center text-gray-600 dark:text-gray-200 mt-5 lg:w-5/12 sm:w-7/12">A good idiom for kids is "It's raining cats and dogs." Kids know what both cats and dogs are from an early age</p>
           </div>
           <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 pb-12 pt-20">
             <div className="w-full flex items-center justify-center flex-col">
               <div className="w-full px-5 -mb-10  relative z-40">
-                <Image
-                  src="https://i.ibb.co/85RFr5V/christina-wocintechchat-com-0-Zx1b-Dv5-BNY-unsplash-1.png"
-                  alt="girl smilling"
-                  className="w-full h-full rounded-md"
-                />
+                <img src="https://i.ibb.co/85RFr5V/christina-wocintechchat-com-0-Zx1b-Dv5-BNY-unsplash-1.png" alt="girl smilling" className="w-full h-full rounded-md" />
               </div>
               <div className="pb-8 pt-14 bg-indigo-700 rounded-md w-full ">
-                <p className="text-xl font-semibold leading-5 text-center text-white">
-                  Anna Rose
-                </p>
-                <p className="text-base leading-4 mt-2 text-center text-white">
-                  District Brand Associate
-                </p>
+                <p className="text-xl font-semibold leading-5 text-center text-white">Anna Rose</p>
+                <p className="text-base leading-4 mt-2 text-center text-white">District Brand Associate</p>
               </div>
             </div>
             <div className="w-full flex items-center justify-center flex-col">
               <div className="w-full px-5 -mb-10  relative z-40">
-                <Image
-                  src="https://i.ibb.co/fNyc2Tg/austin-distel-7uo-Mmz-Pd2-JA-unsplash-1.png"
-                  alt="guy smilling"
-                  className="w-full h-full rounded-md"
-                />
+                <img src="https://i.ibb.co/fNyc2Tg/austin-distel-7uo-Mmz-Pd2-JA-unsplash-1.png" alt="guy smilling" className="w-full h-full rounded-md" />
               </div>
               <div className="pb-8 pt-14 bg-indigo-700 rounded-md w-full ">
-                <p className="text-xl font-semibold leading-5 text-center text-white">
-                  Jack Daniels
-                </p>
-                <p className="text-base leading-4 mt-2 text-center text-white">
-                  Lead Communications Developer
-                </p>
+                <p className="text-xl font-semibold leading-5 text-center text-white">Jack Daniels</p>
+                <p className="text-base leading-4 mt-2 text-center text-white">Lead Communications Developer</p>
               </div>
             </div>
             <div className="w-full flex items-center justify-center flex-col">
               <div className="w-full px-5 -mb-10  relative z-40">
-                <Image
-                  src="https://i.ibb.co/F4pdTRP/michael-dam-m-EZ3-Po-FGs-k-unsplash-1.png"
-                  alt="girl smilling"
-                  className="w-full h-full rounded-md"
-                />
+                <img src="https://i.ibb.co/F4pdTRP/michael-dam-m-EZ3-Po-FGs-k-unsplash-1.png" alt="girl smilling" className="w-full h-full rounded-md" />
               </div>
               <div className="pb-8 pt-14 bg-indigo-700 rounded-md w-full ">
-                <p className="text-xl font-semibold leading-5 text-center text-white">
-                  Cindy Williams
-                </p>
-                <p className="text-base leading-4 mt-2 text-center text-white">
-                  Corporate Integration Engineer
-                </p>
+                <p className="text-xl font-semibold leading-5 text-center text-white">Cindy Williams</p>
+                <p className="text-base leading-4 mt-2 text-center text-white">Corporate Integration Engineer</p>
               </div>
             </div>
           </div>
         </div>
         <div>
-          <div
-            id="button"
-            className="hidden container mx-auto flex justify-center items-center px-10 py-20"
-          >
-            <button
-              onClick="openModal()"
-              className="text-gray-800 font-semibold border rounded-md bg-white py-5 px-10 focus:outline-none hover:bg-gray-200"
-            >
+          <div id="button" className="hidden container mx-auto flex justify-center items-center px-10 py-20">
+            <button onClick="openModal()" className="text-gray-800 font-semibold border rounded-md bg-white py-5 px-10 focus:outline-none hover:bg-gray-200">
               Show Modal
             </button>
           </div>
@@ -226,23 +130,12 @@ function index() {
                     <button onClick="closeModal()" />
                   </div>
                   <p className="text-sm leading-none dark:text-white text-center md:pt-0 pt-3">
-                    Try free for 30 days -{" "}
-                    <u className="no-underline text-indigo-700 dark:text-indigo-400">
-                      No credit card required
-                    </u>
+                    Try free for 30 days - <u className="no-underline text-indigo-700 dark:text-indigo-400">No credit card required</u>
                   </p>
-                  <h1 className="pt-4 pb-10 text-2xl md:text-4xl lg:text-5xl font-bold text-center text-gray-800 dark:text-white ">
-                    Get new products every months in your inbox!
-                  </h1>
+                  <h1 className="pt-4 pb-10 text-2xl md:text-4xl lg:text-5xl font-bold text-center text-gray-800 dark:text-white ">Get new products every months in your inbox!</h1>
                   <div className="flex justify-center flex-col md:flex-row gap-4 md:gap-7">
-                    <input
-                      type="email"
-                      placeholder="Enter you email to join"
-                      className="bg-white dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-700 dark:text-white border rounded-md placeholder-gray-300 border-gray-300 p-4 w-full lg:w-1/2 focus:outline-none"
-                    />
-                    <button className="px-8 py-4 bg-indigo-700 rounded-md text-white font-medium text-base focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:ring-opacity-50 hover:bg-indigo-800 w-full md:w-auto">
-                      Subscribe
-                    </button>
+                    <input type="email" placeholder="Enter you email to join" className="bg-white dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-700 dark:text-white border rounded-md placeholder-gray-300 border-gray-300 p-4 w-full lg:w-1/2 focus:outline-none" />
+                    <button className="px-8 py-4 bg-indigo-700 rounded-md text-white font-medium text-base focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:ring-opacity-50 hover:bg-indigo-800 w-full md:w-auto">Subscribe</button>
                   </div>
                 </div>
               </div>
--- components/page3/index.js
+++ components/page3/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const { v4: uuidv4 } = require("uuid");
 
 function index() {
@@ -19,37 +19,29 @@ function index() {
       setTodoItem("");
     }
   };
-  const [items, setItems] = useState([
-    {
-      id: "1234",
-      message: "Buy Milk",
-      done: false,
-    },
-  ]);
+  const [items, setItems] = useState([]);
+  // remove item from the list
+  const handleRemove = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
 
   return (
     <>
-      <h2 className="text-neutral-700 font-extrabold text-5xl items-center justify-center mx-auto text-center">
-        TO-DO-LIST
-      </h2>
-      <div className="items-center justify-center mx-auto text-center mt-5">
-        <input
-          type="text"
-          className="font-bold text-3xl border-gray-300  border-3 rounded focus:border-gray-500"
-          value={todoItem}
-          onChange={(e) => setTodoItem(e.target.value)}
-        />
-        <button
-          className="font-semibold  ml-5 text-4xl text-gray-600 text-right items-right justify-end ml-auto"
-          onClick={handleAdd}
-        >
-          Add+
-        </button>
-        <ul className="text-2xl font-bold">
+      <h2 className="text-neutral-700 font-extrabold text-5xl items-center justify-center mx-auto text-center">TO-DO-LIST</h2>
+      <div className="flex flex flex-col items-center justify-center mx-auto text-center mt-5">
+        <div className="flex flex-col items-center justify-center w-96">
+          <input placeholder="Enter List" type="text" className="font-bold text-base text-gray-500 focus:outline-none border border-gray-400 rounded-md px-3 py-2 w-full" value={todoItem} onChange={(e) => setTodoItem(e.target.value)} />
+          <button className="font-semibold flex items-center justify-center mt-4 text-sm text-white text-right items-right bg-indigo-700 px-3 py-2 rounded-md w-full" onClick={handleAdd}>
+            Add
+          </button>
+        </div>
+        <ul className="text-2xl font-bold w-96 ">
           {items.map(({ id, message }) => (
-            <li key={id}>{message}</li>
+            <li onClick={() => handleRemove(id)} className="cursor-pointer bg-gray-100 p-2 mt-3 flex items-center justify-start" key={id}>
+              <div className="w-3 h-3 rounded-full bg-gray-300 mr-3 mt-2"></div> {message}
+            </li>
           ))}
-          <li>{todoItem}</li>
+          {items.length > 0 && <p className="text-gray-500 mt-2">Click on them to delete</p>}
         </ul>
       </div>
     </>
