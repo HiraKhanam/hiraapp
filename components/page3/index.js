3bbdff0da7f1c4b7d3b208dc3ef28401c4ab8718
Hira Khanam <hira.alphasquad@gmail.com>
2022-06-17 16:28:53 +0500

    to do

--- components/page3/index.js
+++ components/page3/index.js
@@ -1,50 +1,59 @@
-
 import { useState } from "react";
+const { v4: uuidv4 } = require("uuid");
 
-// First import useState from react hooks
 function index() {
   // name and call the state
-  const [hide, setHide] = useState(true);
-  const [hide1, setHide1] = useState(true);
-  const [hide2, setHide2] = useState(true);
+
+  const [todoItem, setTodoItem] = useState("");
+  const handleAdd = () => {
+    if (todoItem) {
+      setItems([
+        {
+          id: uuidv4(),
+
+          message: todoItem,
+          done: false,
+        },
+        ...items,
+      ]);
+      setTodoItem("");
+    }
+  };
+  const [items, setItems] = useState([
+    {
+      id: "1234",
+      message: "Buy Milk",
+      done: false,
+    },
+  ]);
+
   return (
     <>
-    <div >
-        <h2 className="text-neutral-700 font-extrabold text-6xl items-center justify-center mx-auto text-center"> TO-DO-LIST</h2>
-    <div class="flex items-center justify-center mt-5">
-    {hide && (
-    <div className="container bg-rose-200 p-4 m-4 w-80 flex justify-content-between"><h1 className=" font-bold text-2xl text-neutral-500">Must DO!!</h1>
-    <button  onClick={() => setHide(false)} className="ml-auto"><img className="p-1  cursor-pointer"
-                                                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/horizontal-navigation-svg9.svg"
-                                                alt="cross" /></button>
-        
-
-    </div>)}
-    {hide1 && (
-    <div className="container bg-green-200 p-4 m-4 w-80 flex justify-content-between"><h1 className=" font-bold text-2xl text-neutral-500">Should DO!!</h1>
-    <button  onClick={() => setHide1(false)} className="ml-auto"><img className="p-1  cursor-pointer"
-                                                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/horizontal-navigation-svg9.svg"
-                                                alt="cross" /></button>
-        
-
-    </div>)}
-    {hide2 && (
-    <div className="container bg-blue-200 p-4 m-4 w-80 flex justify-content-between"><h1 className=" font-bold text-2xl text-neutral-500">If i have time</h1>
-    <button  onClick={() => setHide2(false)} className="ml-auto"><img className="p-1  cursor-pointer"
-                                                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/horizontal-navigation-svg9.svg"
-                                                alt="cross" /></button>
-        
-
-    </div>)}
-  
-        
-        
-
-       
-
-    </div></div>
+      <h2 className="text-neutral-700 font-extrabold text-5xl items-center justify-center mx-auto text-center">
+        TO-DO-LIST
+      </h2>
+      <div className="items-center justify-center mx-auto text-center mt-5">
+        <input
+          type="text"
+          className="font-bold text-3xl border-gray-300  border-3 rounded focus:border-gray-500"
+          value={todoItem}
+          onChange={(e) => setTodoItem(e.target.value)}
+        />
+        <button
+          className="font-semibold  ml-5 text-4xl text-gray-600 text-right items-right justify-end ml-auto"
+          onClick={handleAdd}
+        >
+          Add+
+        </button>
+        <ul className="text-2xl font-bold">
+          {items.map(({ id, message }) => (
+            <li key={id}>{message}</li>
+          ))}
+          <li>{todoItem}</li>
+        </ul>
+      </div>
     </>
-    
-    );
-  }
-  export default index;
+  );
+}
+
+export default index;
