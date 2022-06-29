aef1cd2e4ef5f72e7bf90b70e00c89d32af71a8d
Hira Khanam <hira.alphasquad@gmail.com>
2022-06-29 19:21:51 +0500

    Form

--- components/page3/index.js
+++ components/page3/index.js
@@ -35,25 +35,45 @@ function index() {
   };
   const handleUpdateSubmit = (e) => {
     e.preventDefault();
-    setItems(items.map((item) => (item.id === updateId ? { ...item, message: update } : item)));
+    setItems(
+      items.map((item) =>
+        item.id === updateId ? { ...item, message: update } : item
+      )
+    );
     setUpdateInput(false);
     setUpdate("");
     setUpdateId("");
   };
+
   return (
     <>
-      <h2 className="text-neutral-700 font-extrabold text-5xl items-center justify-center mx-auto text-center">TO-DO-LIST</h2>
+      <h2 className="text-neutral-700 font-extrabold text-5xl items-center justify-center mx-auto text-center">
+        TO-DO-LIST
+      </h2>
       <div className="flex flex flex-col items-center justify-center mx-auto text-center mt-5">
         <div className="flex flex-col items-center justify-center w-96">
-          <input placeholder="Enter List" type="text" className="font-bold text-base text-gray-500 focus:outline-none border border-gray-400 rounded-md px-3 py-2 w-full" value={todoItem} onChange={(e) => setTodoItem(e.target.value)} />
-          <button className="font-semibold flex items-center justify-center mt-4 text-sm text-white text-right items-right bg-indigo-700 px-3 py-2 rounded-md w-full" onClick={handleAdd}>
+          <input
+            placeholder="Enter List"
+            type="text"
+            className="font-bold text-base text-gray-500 focus:outline-none border border-gray-400 rounded-md px-3 py-2 w-full"
+            value={todoItem}
+            onChange={(e) => setTodoItem(e.target.value)}
+          />
+          <button
+            className="font-semibold flex items-center justify-center mt-4 text-sm text-white text-right items-right bg-indigo-700 px-3 py-2 rounded-md w-full"
+            onClick={handleAdd}
+          >
             Add
           </button>
         </div>
         <ul className="text-2xl font-bold w-96 ">
           {items.map(({ id, message }) => (
-            <li className="cursor-pointer bg-gray-100 p-2 mt-3 flex items-center justify-start" key={id}>
-              <div className="w-3 h-3 rounded-full bg-gray-300 mr-3 mt-2"></div> {message}
+            <li
+              className="cursor-pointer bg-gray-100 p-2 mt-3 flex items-center justify-start"
+              key={id}
+            >
+              <div className="w-3 h-3 rounded-full bg-gray-300 mr-3 mt-2"></div>{" "}
+              {message}
               <button className="ml-auto" onClick={() => handleRemove(id)}>
                 Remove
               </button>
@@ -69,11 +89,21 @@ function index() {
           ))}
           {updateInput === true && (
             <form onSubmit={handleUpdateSubmit} className="mt-4">
-              <label htmlFor="email" className="text-gray-800 dark:text-gray-100 text-sm font-bold leading-tight tracking-normal mb-2">
+              <label
+                htmlFor="email"
+                className="text-gray-800 dark:text-gray-100 text-sm font-bold leading-tight tracking-normal mb-2"
+              >
                 Update
               </label>
-              <input value={update} onChange={(e) => setUpdate(e.target.value)} className="text-gray-600 dark:text-gray-400 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-700 dark:border-gray-700 dark:bg-gray-800 bg-white font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow" placeholder="Placeholder" />
-              <button className="font-semibold flex items-center justify-center mt-4 text-sm text-white text-right items-right bg-indigo-700 px-3 py-2 rounded-md w-full">Update</button>
+              <input
+                value={update}
+                onChange={(e) => setUpdate(e.target.value)}
+                className="text-gray-600 dark:text-gray-400 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-700 dark:border-gray-700 dark:bg-gray-800 bg-white font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow"
+                placeholder="Placeholder"
+              />
+              <button className="font-semibold flex items-center justify-center mt-4 text-sm text-white text-right items-right bg-indigo-700 px-3 py-2 rounded-md w-full">
+                Update
+              </button>
             </form>
           )}
         </ul>
