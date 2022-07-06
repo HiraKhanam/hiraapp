b5bf9011323059adac4bba054d75932d90d02075
Hira Khanam <hira.alphasquad@gmail.com>
2022-07-06 15:22:47 +0500

    new

--- pages/api1.js
+++ pages/api1.js
@@ -1,7 +1,7 @@
+import image from "next/image";
 export const getStaticProps = async () => {
   const res = await fetch("https://rickandmortyapi.com/api/character");
   const data = await res.json();
-  console.log(data);
 
   return {
     props: {
@@ -12,15 +12,32 @@ export const getStaticProps = async () => {
 
 const api1 = ({ data }) => {
   return (
-    <div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 bg-pink-200 border">
       {data.results.map((curElem) => {
         return (
-          <div key={curElem.id}>
-            <h3 className="font-extrabold italic">{curElem.id}</h3>
-            <h2 className="font-bold">{curElem.name}</h2>
-            <h2 className="font-semibold">{curElem.status}</h2>
-            <h2>{curElem.species}</h2>
-            <h2 className="italic">{curElem.url}</h2>
+          <div
+            key={curElem.id}
+            className="flex  bg-indigo-400 items-center justify-center p-5"
+          >
+            <img className="my-2 rounded md:w-64 w-40" src={curElem.image} />
+            <div class="ml-2 md:ml-5 my-auto">
+              <h3 className="font-extrabold italic sm:text-lg text-sm text-indigo-200 sm:mb-2 mb-0">
+                {curElem.id}.
+              </h3>
+              <h2 className="font-bold text-xl  text-indigo-100">
+                Name: {curElem.name}
+              </h2>
+              <h2 className="font-semibold text-lg  text-indigo-100">
+                Status:{curElem.status}
+              </h2>
+              <h2 className="font-semibold text-lg  text-indigo-100">
+                Species:{curElem.species}
+              </h2>
+              <h2 className="font-semibold text-lg  text-indigo-100">
+                Gender:{curElem.gender}
+              </h2>
+              <h2 className="italic text-sm  text-indigo-100">{curElem.url}</h2>
+            </div>
           </div>
         );
       })}
