a03ac676cf68819038ba1c1f65ed0eda3d240ee1
Hira Khanam <hira.alphasquad@gmail.com>
2022-07-06 13:01:01 +0500

    new file

--- pages/api1.js
+++ pages/api1.js
@@ -1,24 +1,31 @@
 export const getStaticProps = async () => {
   const res = await fetch("https://rickandmortyapi.com/api/character");
   const data = await res.json();
+  console.log(data);
+
   return {
     props: {
       data,
     },
   };
 };
+
 const api1 = ({ data }) => {
   return (
     <div>
-      {data?.map((curElem) => {
+      {data.results.map((curElem) => {
         return (
           <div key={curElem.id}>
-            <h3>{curElem.id}</h3>
-            <h2>{curElem.name}</h2>
+            <h3 className="font-extrabold italic">{curElem.id}</h3>
+            <h2 className="font-bold">{curElem.name}</h2>
+            <h2 className="font-semibold">{curElem.status}</h2>
+            <h2>{curElem.species}</h2>
+            <h2 className="italic">{curElem.url}</h2>
           </div>
         );
       })}
     </div>
   );
 };
+
 export default api1;
